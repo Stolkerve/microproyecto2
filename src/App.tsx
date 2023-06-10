67afdc09222dca088e1589b2d614a8756d5bf7b9
Stolkerve <devsebasgr@gmail.com>
2023-06-10 14:48:54 -0400

    Layout terminado

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,56 @@ import Signup from "./views/Signup";
 import Profile from "./views/Profile";
 import Movie from "./views/Movie";
 import Admin from "./views/Admin";
+import { useEffect, useState } from "react";
+import { User, onAuthStateChanged } from "firebase/auth";
+import { firebaseAuth } from "./firebase/client";
+import { useGlobalState } from "./globalState";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
+  const [user, setUser] = useGlobalState("user")
+  const [loadingAuth, setLoadingAuth] = useState(true)
+
+  useEffect(() => {
+    onAuthStateChanged(firebaseAuth, (u) => {
+      setUser(u)
+      setLoadingAuth(false)
+    })
+  }, [])
+
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/movie" element={<Movie />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="*" element={<Navigate to={"/"} />} />
-      </Routes>
+      {
+        loadingAuth ?
+          <></> : (
+          <div>
+            <Navbar/>
+            <Routes>
+              <Route path="/" element={<Landing />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/movie" element={<Movie />} />
+              {/* { user ? <PrivateRoutes /> : <Navigate to={"/"} /> } */}
+
+              <Route path="*" element={<Navigate to={"/"} />} />
+
+            </Routes>
+            <Footer/>
+          </div>
+        )
+      }
     </div>
   );
+}
+
+function PrivateRoutes() {
+  return (
+    <>
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/admin" element={<Admin />} />
+    </>
+  )
 }
 
 export default App;
--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -9,7 +10,6 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
-console.log(firebaseConfig);
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const firebaseApp = initializeApp(firebaseConfig);
+export const firebaseAuth = getAuth(firebaseApp);
