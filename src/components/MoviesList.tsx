74ba5a3be5350cef75fe9f70ff32cec8c58efadc
Stolkerve <devsebasgr@gmail.com>
2023-06-11 21:14:06 -0400

    Carta de las peliculas

--- src/components/MoviesList.tsx
+++ src/components/MoviesList.tsx
@@ -58,10 +58,23 @@ const MoviesList = () => {
           <FaSearch/>
         </button>
       </form>
-      <div>
+      <div className="movies-card-container">
         {movies.map((m) => {
           return (
-            <></>
+            <div className="movie-card">
+              <img className="movie-card-img" src={`https://image.tmdb.org/t/p/w200/${m.posterPath}`}/>
+              <div className="movie-card-conteiner">
+                <div style={{fontWeight: "500", fontSize: "20px"}}>{m.title}</div>
+                <div style={{maxHeight: "120px", overflow: "scroll"}}>{m.overview}</div>
+                <div style={{marginTop: "auto", display: "flex", flexWrap: "wrap", alignItems: "center", gap: "1rem"}}>
+                  <div style={{fontSize: "14px", fontWeight: "500"}}>{m.originalLanguage}</div>
+                  {m.genreIds.map((g) => {
+                   return <div style={{fontSize: "14px", fontWeight: "500", backgroundColor: "gray", padding: "0.25rem", borderRadius: "14px"}}>{g}</div>
+                  })
+                  }
+                </div>
+              </div>
+            </div>
           )
         })}
       </div>
