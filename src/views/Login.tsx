d50d267f822772acdf2bbede672afc4a8c2fc0da
Andres Bucarello <andres.bucarello@correo.unimet.edu.ve>
2023-06-11 20:17:29 -0400

    Login,Signup,user

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,7 +1,32 @@
 import React from "react";
-
+import "./login.css"
 const Login = () => {
-  return <div>Login</div>;
+  return `<form class="formulario">
+  <div class="contenedor">
+      <header class="header">
+          <div class="menu">
+              <h6><a href="" style="color: white;">MENU</a></h6>
+          </div>
+          <div class="title">
+              <h1>¿POSEES UNA CUENTA? INICIA SESION</h1>
+          </div>    
+      </header>
+      <div class="input-contenedor"> 
+          <div class="input-correo">
+              <h3><label>Correo Electronico:<br><input type="text" placeholder="Ingresa tu correo"></label></h3>
+          </div>
+          <div class="input-contraseña">
+              <h3><label>Contraseña:<br><input type="password" placeholder="Ingresa tu contraseña"></label></h3>
+          </div>
+      </div>
+      <div class="submit-contenedor">
+          <input type="submit" value="INICIAR SESION" class="button">
+      </div>
+      <footer>
+          <p>¿No tienes cuenta? <a class="link" href="">Registrate</a></p>
+      </footer>
+  </div>
+</form>`;
 };
 
 export default Login;
--- src/views/Profile.tsx
+++ src/views/Profile.tsx
@@ -1,7 +1,62 @@
 import React from "react";
-
+import "./login.css"
 const Profile = () => {
-  return <div>Profile</div>;
+  return `<div class="contenedor">
+  <header class="header">
+      <section class="button"><h3>Menu</h3></section>
+      <section class="button"><h3>Salir</h3></section>
+  </header>
+  <div class="banner">
+      <h1>BIENVENIDO(A) SR(A) CARLOS PEREZ</h1>
+  </div>
+  <div class="favorites-movies-contender">
+      <div class="favorites-movies">
+          <header class="header-favorites-movies">
+              <h2>MIS PELICULAS FAVORITAS</h2>
+          </header>
+          <div class="content-favorites-movies">
+              <div class="movie">
+                  <section class="image">
+                      <img src="" alt="No image" srcset="">
+                  </section>
+                  <section class="info">
+                      <header class="title">
+                          <h3>Titulo</h3>
+                      </header>
+                      <section>
+                          <p class="text"> <img src="" alt="No image" srcset="">  Desmarcar como favorita </p>
+                      </section>
+                  </section>
+              </div>
+          </div>
+      </div>
+  </div>
+ 
+  <div class="bookins-contender">
+      <div class="bookins">
+          <header class="header-bookins">
+              <h2>MIS RESERVACIONES</h2>
+          </header>
+          <div class="content-bookins">
+              <div class="movie2">
+                  <section class="image">
+                      <img src="" alt="No image" srcset="">
+                  </section>
+                  <section class="info2">
+                      <header class="title">
+                          <h3>Titulo</h3>
+                      </header>
+                      <section>
+                          <p class="text"> Pelicula: </p>
+                          <p class="text"> Fecha de funcion: </p>
+                          <p class="text"> Cantidad de Boletos: </p>
+                      </section>
+                  </section>
+              </div>
+          </div>
+      </div>
+  </div>
+</div>`;
 };
 
 export default Profile;
