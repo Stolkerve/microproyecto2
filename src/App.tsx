b5af55fd18c8b94afb81b42e7d1fa66f84ad63eb
Stolkerve <devsebasgr@gmail.com>
2023-06-12 13:39:13 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ function App() {
             <Route path="/" element={<Landing />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
-            <Route path="/movie" element={<Movie />} />
+            <Route path="/movie/:id" element={<Movie />} />
             {user ? (
               <>
                 <Route path="/profile" element={<Profile />} />
