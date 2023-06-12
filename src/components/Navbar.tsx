ad19054077cd948ec0ccd4952feab776d7a45c64
Stolkerve <devsebasgr@gmail.com>
2023-06-12 13:39:31 -0400

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,16 +2,29 @@ import React from "react";
 import "./navbar.css";
 import logo from "../assets/cinex2.webp";
 import { useNavigate } from "react-router-dom";
+import { useGlobalState } from "../globalState";
+import { firebaseAuth } from "../firebase/client";
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const [user] = useGlobalState("user");
 
   return (
     <nav>
       <img className="nav-logo" onClick={() => navigate("/")} alt="Logo" src={logo} />
       <div style={{ gap: "1rem", display: "flex" }}>
-        <button onClick={() => navigate("/signup")}>Registrarse</button>
-        <button onClick={() => navigate("/login")}>Ingresar</button>
+        {user ? (
+            <>
+              <button onClick={() => navigate("/profile")}>Perfil</button>
+              <button onClick={() => firebaseAuth.signOut()}>Salir</button>
+            </>
+          ) : (
+            <>
+              <button onClick={() => navigate("/signup")}>Registrarse</button>
+              <button onClick={() => navigate("/login")}>Ingresar</button>
+            </>
+          )
+        }
       </div>
     </nav>
   );
